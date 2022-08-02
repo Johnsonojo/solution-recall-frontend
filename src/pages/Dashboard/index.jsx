278b2b49572bfffc6ee76901b20acaf2a014e641
johnsonojo <johnsonojo89@gmail.com>
2022-08-02 16:55:41 +0000

    add all post page

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import MainNavbar from "../../components/MainNavbar";
+import PostCard from "../../components/PostCard";
 
 const DashboardPage = () => {
   const navigate = useNavigate();
@@ -21,6 +22,7 @@ const DashboardPage = () => {
       >
         Create Post
       </Button>
+      <PostCard />
     </div>
   );
 };
--- src/pages/Post/allPosts/index.jsx
+++ src/pages/Post/allPosts/index.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import { useQuery, useQueryClient } from "react-query";
 import MainNavbar from "../../../components/MainNavbar";
+import PostCard from "../../../components/PostCard";
 import postAPI from "../../../redux/api/postAPI";
 import queryKeys from "../../../redux/api/queryKeys";
 
@@ -8,7 +12,6 @@ const AllPosts = () => {
   const [allPosts, setAllPosts] = useState([]);
 
   const queryClient = useQueryClient();
-  // const navigate = useNavigate();
 
   const allPostsForUser = useQuery([queryKeys.allPosts], postAPI.getAllPosts, {
     onSuccess: (response) => {
@@ -23,16 +26,27 @@ const AllPosts = () => {
   });
 
   const { error, isError, isLoading } = allPostsForUser;
-  console.log("allPosts", allPosts);
+  // console.log("allPosts", allPosts);
 
   return (
     <div>
       <MainNavbar />
       <h3 className="mt-3">All Posts</h3>
-      <div className="col-sm-12 col-md-6 col-lg-4 container">
-        {isLoading && <div>Loading...</div>}
-        {isError && <div>{error}</div>}
-      </div>
+      <Container>
+        <Row xs={1} md={2} className="g-6">
+          {allPosts.length === 0 ? (
+            <div>No Posts </div>
+          ) : (
+            allPosts?.map((post) => (
+              <Col xs={12} md={6} lg={4} key={post?.id}>
+                {isLoading && <div>Loading...</div>}
+                {isError && <div>{error}</div>}
+                <PostCard eachProblem={post} />
+              </Col>
+            ))
+          )}
+        </Row>
+      </Container>
     </div>
   );
 };

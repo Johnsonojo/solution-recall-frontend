64960b0ba4e1e7ab3b6fd72e465d92272fea67cb
Johnson Ojo <johnsonojo89@gmail.com>
2022-10-29 19:48:54 +0100

    update search page

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,9 +1,4 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
 import { useQuery, useQueryClient } from "react-query";
 import MainNavbar from "../../components/MainNavbar";
 import PostCard from "../../components/PostCard";
@@ -31,56 +26,80 @@ const SearchPost = () => {
     }
   );
 
+  const renderSearchPosts = () => {
+    return (
+      <div className="container">
+        {isError ? (
+          <div className="row mt-5">
+            <div className="text-center">
+              <h4>No post found</h4>
+            </div>
+          </div>
+        ) : (
+          <div className="row g-6 mt-5">
+            {allPosts?.map((post) => (
+              <div className="col-sm-12 col-md-6 col-lg-4 mb-4" key={post?.id}>
+                {isError && <div>{error}</div>}
+                <PostCard eachProblem={post} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  };
   return (
     <div>
       <MainNavbar />
       <h3 className="mt-4 mb-4">Recall a solution</h3>
-      <Container>
-        <Row className="mt-4">
-          <Col xs={12} md={6} lg={4}></Col>
 
-          <Col xs={12} md={6} lg={4}>
-            <Form className="d-flex">
-              <Form.Control
-                type="text"
-                placeholder="Enter keyword"
-                className="col-sm-4"
-                aria-label="Search"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-              <Button
-                variant="outline-dark"
-                className="col-sm-3"
-                disabled={isLoading}
-                onClick={refetch}
-              >
-                Recall
-              </Button>
-            </Form>
-          </Col>
-          <Col xs={12} md={6} lg={4}></Col>
-        </Row>
-      </Container>
+      <div className="container col-sm-12 col-md-6">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter keyword"
+            aria-label="Search"
+            aria-describedby="search-button"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button
+            class="btn btn-outline-dark col-sm-3"
+            disabled={isLoading}
+            onClick={() => refetch}
+            type="button"
+            id="search-button"
+          >
+            Recall
+          </button>
+        </div>
+      </div>
 
-      <Container>
-        {!allPosts?.length ? (
-          <Row className="mt-5">
-            <div className="text-center">
-              <h3>No post found</h3>
-            </div>
-          </Row>
-        ) : (
-          <Row xs={1} md={2} className="g-6 mt-5">
-            {allPosts?.map((post) => (
-              <Col xs={12} md={6} lg={4} key={post?.id} className="mb-4">
-                {isError && <div>{error}</div>}
-                <PostCard eachProblem={post} />
-              </Col>
-            ))}
-          </Row>
-        )}
-      </Container>
+      {/* <div className="container mt-4 d-flex">
+        <div className="col-sm-12 col-md-6">
+          <form className="d-flex">
+            <input
+              type="text"
+              placeholder="Enter keyword"
+              className="col-sm-4"
+              aria-label="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button
+              type="button"
+              className="btn btn-outline-dark col-sm-3"
+              disabled={isLoading}
+              onClick={() => refetch}
+            >
+              Recall
+            </button>
+          </form>
+        </div>
+      </div> */}
+
+      {renderSearchPosts()}
     </div>
   );
 };
